adc39bb42fbb1f13ad21138ce052447f2603b266
Func <Funcer@outlook.com>
2023-09-14 15:47:02 +0800

    Update DownloadForeignFileJob.php

--- includes/DownloadForeignFileJob.php
+++ includes/DownloadForeignFileJob.php
@@ -56,8 +56,9 @@ class DownloadForeignFileJob extends Job {
 		}
 
 		$response = $this->httpRequestFactory->get( $fileUrl, [
+			'timeout' => 60,
 			'userAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-		] );
+		], __METHOD__ );
 		if ( !$response ) {
 			wfDebugLog( 'HybridFileRepo', "Download job get URL failed: $fileUrl" );
 			return false;
